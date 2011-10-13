8aceb997bb6c7513492d23735595ba6eec99fd8c
Chris LaRose <cjlarose@gmail.com>
2011-10-12 19:03:17 -0700

    Added custom post type & taxonomy

--- models/event-posts.php
+++ models/event-posts.php
@@ -8,6 +8,38 @@ class WP_Meetup_Event_Posts extends WP_Meetup_Model {
 	parent::__construct();
         global $wpdb;
         $this->wpdb = &$wpdb;
+	
+	/* Create custom post type and taxonomy if appropriate */
+	$this->import_model('options');
+	if ($this->options->get('publish_option') == 'cpt') {
+	    
+	    register_post_type( 'wp_meetup_event',
+		array(
+			'labels' => array(
+				'name' => __( 'Meetup Events' ),
+				'singular_name' => __( 'Meetup Events' )
+			),
+		'public' => true,
+		'has_archive' => true,
+		'supports' => array('title', 'editor', 'thumbnail', 'revisions', 'custom-fields', 'comments'),
+		'rewrite' => array('slug' => 'events'),
+		'show_ui' => TRUE //FALSE
+		)
+	    );
+	    
+	    register_taxonomy('wp_meetup_group', array('wp_meetup_event'), array(
+		'hierarchical' => FALSE,
+		'labels' => array(
+		    'name' => __('Groups'),
+		    'singular_name' => __('Group')
+		),
+		'show_ui' => true,
+		'query_var' => true,
+		'rewrite' => array( 'slug' => 'group' ),
+	    ));
+	    
+	}
+	
     }
     
     private function get_post_status($event_adjusted_time, $publish_buffer, $set_drafts = TRUE) {
@@ -32,17 +64,6 @@ class WP_Meetup_Event_Posts extends WP_Meetup_Model {
         
         $event_adjusted_time = $event->time + $event->utc_offset;
         $post_status = ($event->post_id) ? $event->post->post_status : $this->get_post_status($event_adjusted_time, $publish_buffer);
-        
-        /*$description = "<div class=\"wp-meetup-event\">";
-        $description .= "<a href=\"{$event->event_url}\" class=\"wp-meetup-event-link\">View event on Meetup.com</a>";
-        $description .= "<dl class=\"wp-meetup-event-details\">";
-        $description .= "<dt>Date</dt><dd>" . date("l, F j, Y, g:i A", $event_adjusted_time) . "</dd>";
-        $description .= ($event->venue) ? "<dt>Venue</dt><dd>" .  $event->venue->name . "</dd>" : "";
-        $description .= "</dl>";
-        if ($show_plug)
-            $description .= "<p class=\"wp-meetup-plug\">Meetup.com integration powered by <a href=\"http://nuancedmedia.com/\">Nuanced Media</a>.</p>";
-        $description .= "</div>";
-        $description .= $event->description;*/
 
         $post = array(
             'post_category' => array($category_id),
@@ -72,42 +93,16 @@ class WP_Meetup_Event_Posts extends WP_Meetup_Model {
     }
     
     function remove($post_id = FALSE) {
-	//$this->pr("Time to update post dates");
-	//$posts = $this->get_all();
-	
-	//foreach ($posts_ids as $post_id) {
-	    wp_delete_post($post_id);
-	//}
-	
+	wp_delete_post($post_id);
     }
     
-    /*function recategorize($post_id, $old_category_id, $new_category_id) {
-        //pr($post_id, $old_category_id, $new_category_id);
-        $categories = wp_get_post_categories($post_id);
-        if ($old_category_id) {
-            foreach ($categories as $key => $category) {
-                if ($category == $old_category_id) unset($categories[$key]);
-            }
-        }
-        $categories[] = $new_category_id;
-        
-        $new_post = array(
-            'ID' => $post_id,
-            'post_category' => $categories
-        );
-        wp_update_post($new_post);
-    }*/
-    
     function recategorize($post_id, $category_id) {
-        //pr($post_id, $category_id);
         $new_post = (array) get_post($post_id);
-        //pr($new_post);
         $new_post['post_category'] = array($category_id);
         wp_update_post($new_post);
     }
     
     function set_date($post_id, $event_time, $event_utc_offset, $publish_buffer) {
-	//$this->pr($post_id, $event_time, $event_utc_offset, $publish_buffer);
 	$event_adjusted_time = $event_time + $event_utc_offset;
         $post_status = $this->get_post_status($event_adjusted_time, $publish_buffer, FALSE);
 	
@@ -118,7 +113,6 @@ class WP_Meetup_Event_Posts extends WP_Meetup_Model {
             'post_modified' => current_time( 'mysql' ),
             'post_modified_gmt' => current_time( 'mysql', 1 )
         );
-        //$this->pr($new_post);
 
         $this->wpdb->update($this->wpdb->posts, $new_post, array('ID' => $post_id), array('%s','%s','%s','%s','%s'), array('%d'));
 	
